8ad3cea99e09df83430673d9af64ec5b5dc9cb3e
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-07-27 20:36:24 +0300

    Updates

---
+++ precache-manifest.6a3729e3357a4d116446d6d5177d9f51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf86595a20820b76f1cb77b7d6907f10",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "2b5b27e7896620007521",
+    "url": "/react-carousel/static/css/main.363e03ca.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-carousel/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2b5b27e7896620007521",
+    "url": "/react-carousel/static/js/main.5b92091c.chunk.js"
+  },
+  {
+    "revision": "7f7feaa79c4ebfe5916f",
+    "url": "/react-carousel/static/js/runtime~main.9f3c6066.js"
+  }
+]);
